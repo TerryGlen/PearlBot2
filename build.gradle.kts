e068747d6d6f834cd1751de6102313b250d67701
Terry <saispas99@gmail.com>
2020-10-21 21:37:32 -0500

    Updated Gradle to kts

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+application{
+    mainClass.set("Pearlbot2.main")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit", "junit", "4.12")
+    implementation("com.jessecorbett:diskord-jvm:1.7.3")
+    implementation("org.jetbrains.exposed:exposed-core:0.25.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.25.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.25.1")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.25.1")
+    implementation("mysql:mysql-connector-java:5.1.34")
+}
